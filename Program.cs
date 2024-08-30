99122104ea33c6aa399ac05a94b74c95b00e51b9
Lucas Feuser <lamentavelmentemente@outlook.com.br>
2024-08-29 22:24:47 -0300

    tryGet Pattern

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+      Console.WriteLine("TryGet Pattern..."); 
+      Console.WriteLine("Input same name: ");
+      var name = Console.ReadLine();
+
+      Console.WriteLine(WithTryGetPatternPersonByName(name, out var person) 
+          ? $"Selected person: {person.Name}"
+          : $"Person {name} not found");
+
+       var _person = WithouthTryGetPatternPersonByName(name);
+      Console.WriteLine($"Select person: {_person.Name}");
+
+      Console.ReadKey();
+    }
+
+    static bool WithTryGetPatternPersonByName(string name, out Person person)
+    {
+      var persons = CreatePersons();
+
+      person = persons
+        .FirstOrDefault(_ => _.Name.Equals("name"));
+
+      return person is not null;
+    }
+
+    //Possible NullReference
+    static Person WithouthTryGetPatternPersonByName(string name)
+    {
+      var persons = CreatePersons();
+
+      var result = persons
+        .FirstOrDefault(_ => _.Name.Equals("name"));
+
+      return result;
+    }
+
+     static List<Person> CreatePersons()
+    {
+      return new List<Person>(5)
+      {
+        new() { Name = "Feuser" },
+        new() { Name = "Lucas" },
+        new() { Name = "Amanda" },
+        new() { Name = "Braga" },
+        new() { Name = "Sakura" },
+      };
+    }
+}
+
+public class Person
+{
+  public string Name { get; set; }
+  public int Age { get; set; }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+## TryGet Pattern
+
+
